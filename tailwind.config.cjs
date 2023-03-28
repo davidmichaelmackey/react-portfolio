428fba5b5a7f4392ece51f7748a1a25b6e7def4d
David Mackey <davidmackey92@gmail.com>
2023-03-28 17:13:47 -0500

    tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,14 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // fontFamily: {
+      //   sao: [ 'Sao Torpes', 'sans-serif' ]
+      // }
+    }
   },
   plugins: [],
 };
