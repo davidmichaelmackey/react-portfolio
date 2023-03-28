08db6e704aaf00d557fc326d7fa2bbb7f170fa63
David Mackey <davidmackey92@gmail.com>
2023-03-28 17:15:25 -0500

    contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import gradient2 from '../assets/images/gradient2.jpg';
+
+const Contact = () => {
+  return (
+    <div>
+      <img
+        className='w-full h-screen object-cover object-center'
+        src={gradient2} //!!!
+        alt='A colorful gradient background photo.'
+      />
+      <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+        <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+          <h1 className='mb-48 py-4 text-6xl font-bold text-center text-[#001B5E]'>
+            Contact
+          </h1>
+          <form
+            action='https://getform.io/f/661d5980-90a7-45e8-8609-e1f22db1bc81'
+            method='POST'
+            encType='multipart/form-data'
+          >
+            <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+              <div className='flex flex-col py-2'>
+                <label className='uppercase text-sm py-2 text-grey-800'>
+                  Name
+                </label>
+                <input
+                  className='bg-transparent border-2 rounded-lg p-3 flex border-[#001B5E] focus:outline-none'
+                  type='text'
+                  name='name'
+                />
+              </div>
+              <div className='flex flex-col py-2'>
+                <label className='uppercase text-sm py-2 text-grey-800'>
+                  Phone
+                </label>
+                <input
+                  className='bg-transparent border-2 rounded-lg p-3 flex border-[#001B5E] focus:outline-none'
+                  type='text'
+                  name='phone'
+                />
+              </div>
+            </div>
+            <div className='grid gap-4 w-full py-2'>
+              <div className='flex flex-col'>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2 text-grey-800'>
+                    Email
+                  </label>
+                  <input
+                    className='bg-transparent border-2 rounded-lg p-3 flex border-[#001B5E] focus:outline-none'
+                    type='email'
+                    name='email'
+                  />
+                </div>
+                <br></br>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2 text-grey-800'>
+                    Subject
+                  </label>
+                  <input
+                    className='bg-transparent border-2 rounded-lg p-3 flex border-[#001B5E] focus:outline-none'
+                    type='text'
+                    name='subject'
+                  />
+                </div>
+                <br></br>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2 text-grey-800'>
+                    Message
+                  </label>
+                  <textarea
+                    className='bg-transparent border-2 rounded-lg p-3 border-[#001B5E] focus:outline-none'
+                    rows='10'
+                    name='message'
+                  ></textarea>
+                </div>
+                <br></br>
+                <div className='flex flex-col py-2'>
+                  <button className='bg-[#001B5E] text-gray-100 mt-4 w-full p-4 rounded-lg'>
+                    Send Message
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
