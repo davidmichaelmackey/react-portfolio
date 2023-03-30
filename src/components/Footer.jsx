cd00a530b73b60af02252cdf64ac235097d274aa
David Mackey <davidmackey92@gmail.com>
2023-03-30 04:36:08 -0500

    footer sizing - built with vite

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import vite from '../assets/images/vite.svg';
 
 const Footer = () => {
   return (
-    <div className='absolute bottom-10 right-10'>
+    <div className='absolute bottom-7 right-7'>
       <a href='https://vitejs.dev/' target='_blank'>
         <p className='float-left text-xl mr-2 font-medium text-gray-400 hover:line-through hover:decoration-amber-300'>
           Built with <span className='text-amber-300'>Vite</span>
